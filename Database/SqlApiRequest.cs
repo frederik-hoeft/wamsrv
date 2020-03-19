80389969d7e66b90043c99534e7c023de229670c
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2020-03-19 14:20:33 +0100

    Added SqlApiRequest To Communicate With wadbsrv

---
+++ Database/SqlApiRequest.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using washared.DatabaseServer;
+using washared.DatabaseServer.ApiRequests;
+
+namespace wamsrv.Database
+{
+    /// <summary>
+    /// SQL API Request wrapper class. Can hold any SQL API Request.
+    /// </summary>
+    public class SqlApiRequest : ApiRequestBase
+    {
+        private SqlApiRequest(RequestId sqlRequestId, string query, int expectedColumns)
+        {
+            RequestId = sqlRequestId;
+            Query = query;
+            ExpectedColumns = expectedColumns;
+        }
+
+        public static SqlApiRequest Create(RequestId sqlRequestId, string query, int expectedColumns)
+        {
+            return new SqlApiRequest(sqlRequestId, query, expectedColumns);
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
